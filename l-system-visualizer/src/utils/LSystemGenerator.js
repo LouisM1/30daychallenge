c5c089b3aeebcaec7754e951f16b93ce63f57765
LouisM1 <louismcauliffe@icloud.com>
2024-08-28 23:15:51 +0100

    added build file

--- l-system-visualizer/src/utils/LSystemGenerator.js
+++
@@ -1,7 +0,0 @@
-export function generateLSystem(axiom, rules, iterations) {
-  let result = axiom;
-  for (let i = 0; i < iterations; i++) {
-    result = result.split('').map(char => rules[char] || char).join('');
-  }
-  return result;
-}
