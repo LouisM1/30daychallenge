a702485cd42e1d28b368ddbb6c718aea29028353
LouisM1 <louismcauliffe@icloud.com>
2024-08-28 23:00:49 +0100

    last changes

--- l-system-visualizer/src/App.js
+++ l-system-visualizer/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import LSystemVisualizer from './components/LSystemVisualizer';
 import ControlPanel from './components/ControlPanel';
-import Gallery from './components/Gallery';
 import { LSystemProvider } from './contexts/LSystemContext';
 
 const InfoButton = () => {
@@ -39,7 +38,6 @@ function App() {
           <div className="left-panel">
             <h1>L-System Visualizer</h1>
             <ControlPanel />
-            <Gallery />
           </div>
           <div className="right-panel">
             <InfoButton />
