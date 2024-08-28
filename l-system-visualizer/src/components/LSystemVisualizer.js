988ba0f3273f106b3bf22c7ef9655f6bb779e6c6
LouisM1 <louismcauliffe@icloud.com>
2024-08-28 16:08:11 +0100

    added zoom

--- l-system-visualizer/src/components/LSystemVisualizer.js
+++ l-system-visualizer/src/components/LSystemVisualizer.js
@@ -1,10 +1,32 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { LSystemContext } from '../contexts/LSystemContext';
 import { generateLSystem } from '../utils/LSystemGenerator';
 
 const LSystemVisualizer = () => {
   const { lSystem, visualParams } = useContext(LSystemContext);
   const canvasRef = useRef(null);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setDragStart({ x: e.clientX - offset.x, y: e.clientY - offset.y });
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const newOffset = {
+        x: e.clientX - dragStart.x,
+        y: e.clientY - dragStart.y
+      };
+      setOffset(newOffset);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -22,6 +44,11 @@ const LSystemVisualizer = () => {
     const stack = [];
     let stepSize = 5 * visualParams.zoom;
 
+    const drawLine = (x1, y1, x2, y2) => {
+      ctx.moveTo(x1 + offset.x, y1 + offset.y);
+      ctx.lineTo(x2 + offset.x, y2 + offset.y);
+    };
+
     switch (lSystem.name) {
       case "Sierpinski Carpet":
       case "Koch Curve":
@@ -37,10 +64,11 @@ const LSystemVisualizer = () => {
           switch (char) {
             case 'F':
             case 'G':
-              ctx.moveTo(x, y);
-              x += Math.cos(angle * Math.PI / 180) * stepSize;
-              y += Math.sin(angle * Math.PI / 180) * stepSize;
-              ctx.lineTo(x, y);
+              const newX = x + Math.cos(angle * Math.PI / 180) * stepSize;
+              const newY = y + Math.sin(angle * Math.PI / 180) * stepSize;
+              drawLine(x, y, newX, newY);
+              x = newX;
+              y = newY;
               break;
             case '+':
               angle += lSystem.angle;
@@ -74,10 +102,11 @@ const LSystemVisualizer = () => {
           switch (char) {
             case '0':
             case '1':
-              ctx.moveTo(x, y);
-              x += Math.cos(angle * Math.PI / 180) * stepSize;
-              y += Math.sin(angle * Math.PI / 180) * stepSize;
-              ctx.lineTo(x, y);
+              const newX = x + Math.cos(angle * Math.PI / 180) * stepSize;
+              const newY = y + Math.sin(angle * Math.PI / 180) * stepSize;
+              drawLine(x, y, newX, newY);
+              x = newX;
+              y = newY;
               break;
             case '[':
               stack.push({ x, y, angle });
@@ -95,9 +124,20 @@ const LSystemVisualizer = () => {
     }
 
     ctx.stroke();
-  }, [lSystem, visualParams]);
+  }, [lSystem, visualParams, offset]);
 
-  return <canvas ref={canvasRef} width="800" height="600" />;
+  return (
+    <canvas
+      ref={canvasRef}
+      width="800"
+      height="600"
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      style={{ cursor: isDragging ? 'grabbing' : 'grab' }}
+    />
+  );
 };
 
 export default LSystemVisualizer;
