ee6aeb23a7df5eed2806b65a923ad3fbaa113f77
LouisM1 <louismcauliffe@icloud.com>
2024-08-28 15:40:28 +0100

    all functionality for the different sets in place

--- l-system-visualizer/src/components/LSystemVisualizer.js
+++ l-system-visualizer/src/components/LSystemVisualizer.js
@@ -11,63 +11,87 @@ const LSystemVisualizer = () => {
     const ctx = canvas.getContext('2d');
     const { width, height } = canvas;
 
-    // Clear the canvas
     ctx.clearRect(0, 0, width, height);
-
-    // Generate the L-system string
     const lSystemString = generateLSystem(lSystem.axiom, lSystem.rules, lSystem.iterations);
 
-    // Set up initial drawing state
     ctx.strokeStyle = visualParams.lineColor;
     ctx.lineWidth = visualParams.lineThickness;
     ctx.beginPath();
 
-    // Set up the turtle graphics state
-    let x = width / 2;
-    let y = height / 2;
-    let angle = -90; // Start pointing upwards
+    let x, y, angle;
     const stack = [];
-    const stepSize = 5 * visualParams.zoom;
+    let stepSize = 5 * visualParams.zoom;
 
-    // Draw the L-system
-    for (const char of lSystemString) {
-      switch (char) {
-        case 'F':
-        case 'A':
-        case 'B':
-        case '0':
-        case '1':
-          const newX = x + Math.cos(angle * Math.PI / 180) * stepSize;
-          const newY = y + Math.sin(angle * Math.PI / 180) * stepSize;
-          ctx.moveTo(x, y);
-          ctx.lineTo(newX, newY);
-          x = newX;
-          y = newY;
-          break;
-        case 'G':
-          x += Math.cos(angle * Math.PI / 180) * stepSize;
-          y += Math.sin(angle * Math.PI / 180) * stepSize;
-          break;
-        case '+':
-          angle += lSystem.angle;
-          break;
-        case '-':
-          angle -= lSystem.angle;
-          break;
-        case '[':
-          stack.push({ x, y, angle });
-          break;
-        case ']':
-          const state = stack.pop();
-          x = state.x;
-          y = state.y;
-          angle = state.angle;
-          break;
-        case 'X':
-        case 'Y':
-          // Do nothing for these symbols
-          break;
-      }
+    switch (lSystem.name) {
+      case "Sierpinski Carpet":
+      case "Koch Curve":
+      case "Koch Snowflake":
+      case "Moore Fractal":
+      case "Sierpinski Triangle":
+      case "Dragon Curve":
+      case "Fractal Plant":
+        x = width / 2;
+        y = height / 2;
+        angle = 0;
+        for (const char of lSystemString) {
+          switch (char) {
+            case 'F':
+            case 'G':
+              ctx.moveTo(x, y);
+              x += Math.cos(angle * Math.PI / 180) * stepSize;
+              y += Math.sin(angle * Math.PI / 180) * stepSize;
+              ctx.lineTo(x, y);
+              break;
+            case '+':
+              angle += lSystem.angle;
+              break;
+            case '-':
+              angle -= lSystem.angle;
+              break;
+            case '[':
+              stack.push({ x, y, angle });
+              break;
+            case ']':
+              const state = stack.pop();
+              x = state.x;
+              y = state.y;
+              angle = state.angle;
+              break;
+            case 'L':
+            case 'R':
+            case 'X':
+            case 'Y':
+              // These characters don't affect drawing
+              break;
+          }
+        }
+        break;
+      case "Fractal Binary Tree":
+        x = width / 2;
+        y = height;
+        angle = -90;
+        for (const char of lSystemString) {
+          switch (char) {
+            case '0':
+            case '1':
+              ctx.moveTo(x, y);
+              x += Math.cos(angle * Math.PI / 180) * stepSize;
+              y += Math.sin(angle * Math.PI / 180) * stepSize;
+              ctx.lineTo(x, y);
+              break;
+            case '[':
+              stack.push({ x, y, angle });
+              angle -= lSystem.angle;
+              break;
+            case ']':
+              const state = stack.pop();
+              x = state.x;
+              y = state.y;
+              angle = state.angle + lSystem.angle;
+              break;
+          }
+        }
+        break;
     }
 
     ctx.stroke();
--- l-system-visualizer/src/contexts/LSystemContext.js
+++ l-system-visualizer/src/contexts/LSystemContext.js
@@ -3,25 +3,39 @@ import React, { createContext, useState } from 'react';
 export const LSystemContext = createContext();
 
 const presetSystems = {
-  algae: {
-    name: "Algae (AB system)",
-    axiom: "A",
-    rules: { A: "AB", B: "A" },
-    angle: 0,
-    iterations: 5
+  sierpinskiCarpet: {
+    name: "Sierpinski Carpet",
+    axiom: "F",
+    rules: { F: "F+F-F-F-G+F+F+F-F", G: "GGG" },
+    angle: 90,
+    iterations: 3
+  },
+  kochCurve: {
+    name: "Koch Curve",
+    axiom: "F",
+    rules: { F: "F+F-F-F+F" },
+    angle: 60,
+    iterations: 4
+  },
+  kochSnowflake: {
+    name: "Koch Snowflake",
+    axiom: "F++F++F",
+    rules: { F: "F-F++F-F" },
+    angle: 60,
+    iterations: 4
+  },
+  mooreFractal: {
+    name: "Moore Fractal",
+    axiom: "LFL+F+LFL",
+    rules: { L: "-RF+LFL+FR-", R: "+LF-RFR-FL+" },
+    angle: 90,
+    iterations: 3
   },
   binaryTree: {
     name: "Fractal Binary Tree",
     axiom: "0",
     rules: { "1": "11", "0": "1[0]0" },
     angle: 45,
-    iterations: 5
-  },
-  cantorSet: {
-    name: "Cantor Set",
-    axiom: "A",
-    rules: { A: "ABA", B: "BBB" },
-    angle: 0,
     iterations: 5
   },
   sierpinskiTriangle: {
@@ -48,7 +62,7 @@ const presetSystems = {
 };
 
 export const LSystemProvider = ({ children }) => {
-  const [lSystem, setLSystem] = useState(presetSystems.algae);
+  const [lSystem, setLSystem] = useState(presetSystems.sierpinskiCarpet);
   const [visualParams, setVisualParams] = useState({
     lineColor: '#000000',
     lineThickness: 1,
