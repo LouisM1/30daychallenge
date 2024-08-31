85904a215108759bf48c9e06b3bc8121278c1f8a
LouisM1 <louismcauliffe@icloud.com>
2024-08-31 23:50:35 +0100

    finished the build

--- sorting-visualizer/src/App.js
+++ sorting-visualizer/src/App.js
@@ -3,8 +3,8 @@ import SortingVisualizer from './SortingVisualizer';
 
 function App() {
   return (
-    <div className="App" style={{ textAlign: 'center', padding: '20px', maxWidth: '1200px', margin: '0 auto' }}>
-      <h1 style={{ fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif' }}>Sorting Algorithms Visualizer</h1>
+    <div className="App" style={{ padding: '20px' }}>
+      <h1 style={{ fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif', textAlign: 'center' }}>Sorting Algorithms Visualizer</h1>
       <SortingVisualizer />
     </div>
   );
